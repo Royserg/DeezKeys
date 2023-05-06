a5bb4c085a1e09af9da27510652c70d25bae3ab7
Kuba <staniojakub@gmail.com>
2023-05-06 18:44:12 +0300

    chore(): borrow implementation of rdev for macos

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,15 @@
+use std::thread;
+
+use macos::Event;
 // use rdev::{listen, Event, EventType};
 use tauri::{AppHandle, Error, GlobalShortcutManager, Manager};
+
+#[cfg(target_os = "macos")]
+mod macos;
+#[cfg(target_os = "macos")]
+use crate::macos::listen as macos_listen;
+#[cfg(target_os = "macos")]
+use crate::macos::EventType;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -7,27 +17,27 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-// fn callback(event: Event) {
-//     println!("My callback {:?}", event);
-//     match event.name {
-//         Some(string) => {
-//             println!("EventName: {}", &string);
-//         }
-//         None => {
-//             match event.event_type {
-//                 EventType::KeyPress(key) => {
-//                     println!("KeyPress: {:?}", key)
-//                 }
-//                 EventType::KeyRelease(key) => {
-//                     println!("KeyRelease: {:?}", key)
-//                     // let key_str = format!("{:?}", key);
-//                     // emit("KeyRelease", &key_str);
-//                 }
-//                 _ => {}
-//             }
-//         }
-//     }
-// }
+fn macos_callback(event: Event) {
+    println!("My callback {:?}", event);
+    match event.name {
+        Some(string) => {
+            // println!("EventName: {}", &string);
+        }
+        None => {
+            match event.event_type {
+                EventType::KeyPress(key) => {
+                    println!("KeyPress: {:?}", key)
+                }
+                EventType::KeyRelease(key) => {
+                    println!("KeyRelease: {:?}", key)
+                    // let key_str = format!("{:?}", key);
+                    // emit("KeyRelease", &key_str);
+                }
+                _ => {}
+            }
+        }
+    }
+}
 
 fn main() {
     // let q_shortcut = "Q";
@@ -36,6 +46,18 @@ fn main() {
         .setup(|app| {
             // let app_handle = app.app_handle().to_owned();
             // app_handle.get_window("main").unwrap().close().unwrap();
+
+            #[cfg(target_os = "linux")]
+            thread::spawn(|| {
+                println!("We are on linux");
+            });
+
+            #[cfg(target_os = "macos")]
+            thread::spawn(|| {
+                if let Err(error) = macos_listen(macos_callback) {
+                    println!("Error: {:?}", error);
+                }
+            });
 
             // app.global_shortcut_manager()
             //     .register(q_shortcut, move || {
